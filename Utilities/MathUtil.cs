2aafd06913e4c968b9a89d514cb7ca940609c719
klondono <klondono1016@gmail.com>
2020-05-16 19:52:13 -0400

    Add math and string utils

---
+++ Utilities/MathUtil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Utilities
+{
+    public class MathUtil
+    {
+        public double Divide(double num1, double num2)
+        {
+            return (num1 / num2);
+        }
+
+        public double Multiply(double num1, double num2)
+        {
+            return (num1 * num2);
+        }
+
+        public double Subtract(double num1, double num2)
+        {
+            return (num1 - num2);
+        }
+
+        public double Add(double num1, double num2)
+        {
+            return (num1 + num2);
+        }
+
+        public double SquareRoot(double num1)
+        {
+            var closeEnoughValue = .0001;
+
+            //start from zero and move our way up...
+            var guess = 0;
+
+            for(guess = 1; guess <= num1; guess++)
+            {
+                var answer = guess * guess;
+
+                var diff = num1 - answer;
+
+                if (diff <= closeEnoughValue)
+                    break;
+            }
+
+            return guess;
+        }
+    }
+}
---
+++ Utilities/StringUtil.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities
+{
+    public class StringUtil
+    {
+        public bool HasLetter(string str, char letterToSearchFor)
+        {
+            var hasLetter = false;
+
+            for (var i = 0; i < str.Length; i++)
+            {
+                var charVal = str[i];
+
+                if(str[i] == letterToSearchFor)
+                {
+                    hasLetter = true;
+                    break;
+                }
+            }
+
+            return hasLetter;
+        }
+
+        public void PrintBackwards(string str)
+        {
+            var i = str.Length - 1;
+
+            while (i >= 0)
+            {
+                Console.WriteLine(str[i]);
+                i = i - 1;
+                //i -= 1;
+            }
+        }
+
+        public string Capitalize(string str)
+        {
+            var newArray = new char[str.Length];
+
+            for(var i = 0; i < str.Length; i++)
+            {
+                var charVal = str[i];
+                var capitalizedChar = Char.ToUpper(charVal);
+                newArray[i] = capitalizedChar;
+            }
+            
+            return new string(newArray);
+        }
+
+        public string Reverse(string str)
+        {
+            var newArray = new char[str.Length];
+            var index = 0;
+            for (var i = str.Length - 1; i >= 0; i--)
+            {
+                newArray[index] = str[i];
+                index++;
+            }
+
+            return new string(newArray);
+        }
+
+        public bool IsPalindrome(string phrase)
+        {
+            var newStr = Reverse(phrase);
+            return phrase.Equals(newStr);
+        }
+
+        public bool IsPalindrome2(string phrase)
+        {
+            var index = 0;
+            for (var i = phrase.Length - 1; i >= 0; i--)
+            {
+                if (phrase[index] != phrase[i]) {
+
+                    return false;
+                }
+
+                index++;
+            }
+
+            return true;
+        }
+    }
+}
